b95104a26b9eaf2c10679591ea9457cdbb9f028f
Ryan Davis <ryand-ruby@zenspider.com>
2017-07-14 11:17:49 -0600

    Finished phase 1 of #lang RecImpl
    This has a known "bug" in that functions print as racket procedures.
    
    But it is a huge PITA to fix. Checkpoint.

--- main.rkt
+++ main.rkt
@@ -1,35 +1,114 @@
 #lang racket/base
 
+(require (for-syntax syntax/parse racket/base)
+         syntax/parse/define
+         racket/dict
+         racket/string
+         )
+
+(provide #%top
+         #%top-interaction ; stfu
+         #%app
+         true
+         false
+         defun
+         record
+         @
+         function
+         ++
+         (rename-out [rec-mb #%module-begin]
+                     [rec-datum #%datum]
+                     [rec-if if]
+                     [rec-let let]
+                     [rec-zero? zero?]
+                     [rec-plus +]
+                     [rec-empty? empty?]
+                     )
+         )
+
+(define true #t)
+(define false #f)
+
 (module+ test
   (require rackunit))
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(module reader syntax/module-reader
+  RecImpl)
 
-;; Code here
+(define-syntax (rec-mb stx)
+  (syntax-parse stx
+    [(_ (defun stuff ...) ... e:expr)
+     #'(#%module-begin
+        (defun stuff ...) ...
+        e)]))
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+(define-syntax (rec-datum stx)
+  (syntax-parse stx
+    [(_ . n:number) #'(#%datum . n)]
+    [(_ . s:str) #'(#%datum . s)]))
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+(define-syntax (defun stx)
+  (syntax-parse stx
+    [(_ (id:id x:id) body:expr)
+     #'(define (id x) body)]))
+
+(define-syntax (rec-if stx)
+  (syntax-parse stx
+    [(_ c:expr t:expr f:expr)
+     #'(let ([c* c])
+         (if (boolean? c*)
+             (if c* t f)
+             (error 'if "condition used without boolean, got ~v" c*)))]))
+
+(define-syntax (rec-let stx)
+  (syntax-parse stx
+    [(_ ((x:id e:expr)) body:expr)
+     #'(let ((x e)) body)]))
+
+(define-syntax (record stx)
+  (syntax-parse stx
+    [(_ (s:str e:expr) ...)
+     #'(list 'record (cons s e) ...)]))
+
+(define-syntax (@ stx)
+  (syntax-parse stx
+    [(_ r:expr e:str)
+     #'(let ([r* r])
+         (if (list? r*)
+             (dict-ref (cdr r*) e (lambda () (error '@ "~a not found in ~a" e r*)))
+             (error '@ "not a record, got ~a" r*)))]))
+
+(define-simple-macro (rec-zero? e:expr)
+  (let ([e* e])
+    (check-number 'zero? e*)
+    (zero? e*)))
+
+(define-simple-macro (rec-plus a:expr b:expr)
+  (let ([a* a]
+        [b* b])
+    (check-number '+ a*)
+    (check-number '+ b*)
+    (+ a* b*)))
+
+(define-simple-macro (function id:id)
+  (if (procedure? id)
+      id
+      (error 'function "~v is not a function" id)))
+
+(define-simple-macro (rec-empty? e:expr)
+  (let ([e* e])
+    (check-string 'empty? e*)
+    (not (non-empty-string? e*))))
+
+(define-simple-macro (++ a:expr b:expr)
+  (let ([a* a]
+        [b* b])
+    (check-string '+ a*)
+    (check-string '+ b*)
+    (string-append a* b*)))
+
+(define (check-number fn v)
+  (unless (number? v) (error fn "~v is not a number" v)))
+
+(define (check-string fn v)
+  (unless (string? v) (error fn "~v is not a string" v)))
